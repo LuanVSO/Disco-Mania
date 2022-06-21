68be15d12ea5e37951ef50c871329ea96e25ed0f
Luan Vitor Simião Oliveira <luanv.oliveira@outlook.com>
2022-06-20 21:05:14 -0300

    clean up

--- app/src/main/java/br/fmu/fellas/discomania/MainActivity.java
+++ app/src/main/java/br/fmu/fellas/discomania/MainActivity.java
@@ -4,17 +4,10 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import nl.hypothermic.javacogs.*;
-import nl.hypothermic.javacogs.authentication.TokenAuthenticationMethod;
-
-
 public class MainActivity extends AppCompatActivity {
-
-    final Javacogs client = new Javacogs();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        client.setAuthenticationMethod(new TokenAuthenticationMethod(getString(R.string.discogsToken)));
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         var productClickListener = new PruductClickListener();
